a6c89d53409fce3768af9e3db3fe79a5d09e4efc
AndreAlexander-SM <124051521@upq.edu.mx>
2025-11-06 00:35:49 -0600

    Update MenuScreens.js

--- AhorraAPP/Screens/MenuScreens.js
+++ AhorraAPP/Screens/MenuScreens.js
@@ -40,4 +40,7 @@ export default function Menu() {
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f5f5f5', padding: 20 },
+  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+});
