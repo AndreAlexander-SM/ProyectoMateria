82a14703722c5798354f15146a4c80a1c39d227e
MoonieSof <mooniesofi@gmail.com>
2025-11-06 13:40:41 -0600

    .

--- AhorraAPP/Screens/Graficas.js
+++ AhorraAPP/Screens/Graficas.js
@@ -1,286 +1,269 @@
-import { Text,StyleSheet, View, TouchableOpacity, SafeAreaView } from 'react-native'
+import { Text, StyleSheet, View, TouchableOpacity, SafeAreaView, Image } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons';
-import {Image}from 'react-native';
 
-
-export default function Graficas(){
-
-    return (
+export default function Graficas() {
+  return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.row} >    
 
-         <TouchableOpacity> 
+     
+   <View style={styles.row}>
+        <TouchableOpacity>
           <FontAwesome name="arrow-left" size={30} color="rgba(27, 60, 177, 1)" />
-         </TouchableOpacity>
+        </TouchableOpacity>
 
-        <Text style={styles.encabezado}>  Gráficas </Text>
+        <Text style={styles.encabezado}>Gráficas</Text>
 
         <TouchableOpacity>
           <FontAwesome name="question-circle" size={30} color="#333" />
-            
         </TouchableOpacity>
- 
+      </View>
 
-     </View>
+      <View style={styles.linea} />
 
-     <View style ={styles.linea}/> 
+      <Text style={styles.texto}>Hola, usuario...</Text>
 
-
-        <Text style={styles.texto}>  Hola,usuario...  </Text>
-
-     <View style={styles.row1} >    
-       <View style={styles.imagenPastel}> 
-          <Image source={{uri:'https://matemovil.com/wp-content/uploads/2018/08/gr%C3%A1fico-de-torta-gr%C3%A1fico-circular-pastel.jpg'}} 
-           style={styles.pastel}
-           resizeMode="contain"
-         />
-
-        </View>   
-
-        <View style={styles.categorias}>  
-           <View style={styles.itemCategoria}>
-             <View style={[styles.colorBox, { backgroundColor: '#63beffff' }]} /> 
-            <Text style={styles.nombreCategoria}>Compras</Text>
-         </View>
-
-        <View style={styles.itemCategoria}>
-             <View style={[styles.colorBox, { backgroundColor: '#36a2eb' }]} /> 
-            <Text style={styles.nombreCategoria}>Servicios</Text>
+   
+      <View style={styles.row1}>
+        <View style={styles.imagenPastel}>
+          <Image
+            source={require('../assets/pastel.png')}
+            style={styles.pastel}
+            resizeMode="contain"
+          />
         </View>
-         </View>
 
-
-
-
+       <View style={styles.categorias}>
+          <View style={styles.itemCategoria}>
+            <View style={[styles.colorBox, { backgroundColor: '#71e3ebff' }]} />
+            <Text style={styles.nombreCategoria}>Compras</Text>
           </View>
 
-          <View style={styles.row2}>
-            <Image 
-              source={{uri:'https://help.qlik.com/es-ES/cloud-services/Subsystems/Hub/Content/Resources/Images/ui_gen_BarChart.png'}}
-               style={styles.barras}
-               resizeMode='contain'
-  />
-  <TouchableOpacity style={styles.botonMes}>
-    <Text style={styles.textoBoton}>Mes</Text>
-  </TouchableOpacity>
+          <View style={styles.itemCategoria}>
+            <View style={[styles.colorBox, { backgroundColor: '#4698c1ff' }]} />
+            <Text style={styles.nombreCategoria}>Comida</Text>
+          </View>
+          
+          <View style={styles.itemCategoria}>
+            <View style={[styles.colorBox, { backgroundColor: 'rgba(73, 116, 185, 0.98)' }]} />
+            <Text style={styles.nombreCategoria}>Servicios</Text>
+          </View>
 
+          <View style={styles.itemCategoria}>
+            <View style={[styles.colorBox, { backgroundColor: '#255490ff' }]} />
+            <Text style={styles.nombreCategoria}>Entretenimiento</Text>
+          </View>
 
-</View>
+          <View style={styles.itemCategoria}>
+            <View style={[styles.colorBox, { backgroundColor: '#3a4a81ff' }]} />
+            <Text style={styles.nombreCategoria}>Otros</Text>
+          </View>
 
-         <View style={styles.texto}>
-  <View style={styles.itemColor}>
-    <View style={[styles.colorBox, { backgroundColor: '#ff6b6b' }]} />
-    <Text style={styles.nombreColor}>Gastos</Text>
-  </View>
+        </View>
+      </View>
 
-  <View style={styles.itemColor}>
-    <View style={[styles.colorBox, { backgroundColor: '#1dd1a1' }]} />
-    <Text style={styles.nombreColor}>Ahorros</Text>
-  </View>
-
-  <View style={styles.itemColor}>
-    <View style={[styles.colorBox, { backgroundColor: '#feca57' }]} />
-    <Text style={styles.nombreColor}>Ingresos</Text>
-  </View>
-</View>
    
-    <View style ={styles.linea}/>
-     
+      <View style={styles.row2}>
+        <View style={styles.imagenBarras}>
+          <Image
+            source={require('../assets/barras.png')}
+            style={styles.barras}
+            resizeMode="contain"
+          />
+         
+          <TouchableOpacity style={styles.botonMes}>
+            <Text style={styles.textoBoton}>Mes</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
 
-     <Text style={styles.texto}> 
-           Tus ingresos superan tus gastos en $300 este mes, ¡Sigue así!
+ 
+      <View style={styles.listaInfo}>
+        <View style={styles.itemColor}>
+          <View style={[styles.colorBox, { backgroundColor: '#495cb1ff' }]} />
+          <Text style={styles.nombreColor}>Gastos</Text>
+          <Text style={styles.puntos}>...</Text>
+        </View>
 
-     </Text>
-          
-        </SafeAreaView>
-          
-      
-    )
+        <View style={styles.itemColor}>
+          <View style={[styles.colorBox, { backgroundColor: '#cfccc9ff' }]} />
+          <Text style={styles.nombreColor}>Ahorros</Text>
+          <Text style={styles.puntos}>...</Text>
+        </View>
 
+        <View style={styles.itemColor}>
+          <View style={[styles.colorBox, { backgroundColor: '#ffffff' }]} />
+          <Text style={styles.nombreColor}>Balance</Text>
+          <Text style={styles.puntos}>...</Text>
+        </View>
+      </View>
 
+      <View style={styles.linea} />
 
-
+      <Text style={styles.textoFinal}>
+        Tus ingresos superan tus gastos en $300 este mes, ¡Sigue así!
+      </Text>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingTop: 10,
+  },
 
-    container:{
-        flex:1,
-        justifyContent:'flex-start',
-        paddingTop:10, 
+  row: {
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'space-between',
+    position: 'relative',
+    marginTop: 15,
+  },
 
-
-    },
-
- row:{
-   flexDirection:'row',
-  paddingHorizontal:15,
-  alignItems:'center',
-  width:'100%',
-  justifyContent:'space-between',
-  position:'relative',
-  marginTop:15,
-},
-
-
-   
-
-
-encabezado:{
-
-fontSize: 30,
+  encabezado: {
+    fontSize: 30,
     color: '#151313ff',
     fontWeight: 'bold',
-    position:'absolute',
-    left:0,
-    right:0,
-    textAlign:'center',
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    textAlign: 'center',
     marginTop: 8,
-   
+  },
 
-},
+  linea: {
+    height: 2,
+    backgroundColor: '#393737ff',
+    marginVertical: 20,
+    width: '100%',
+    alignSelf: 'center',
+  },
 
-button:{ 
-fontSize:20,
-fontWeight:'bold',
-marginBottom:10,
-color:'blue',
+  texto: {
+    color: 'gray',
+    fontSize: 20,
+    marginTop: 10,
+    marginLeft: 25,
+  },
 
-},
+  row1: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
 
-imagenPastel:{
-    marginTop: 40,
-    alignSelf:'flex-start',
-    
-   
-    
+  imagenPastel: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex:1,
+    marginLeft:40,
+  },
 
-
-},
-
-pastel:{
+  pastel: {
     width: 200,
-    height:200,
-    
-    
+    height: 200,
+  },
 
-},
+  categorias: {
+    marginLeft: 40,
+    justifyContent: 'center',
+      marginLeft: 40,
+  },
 
-texto:{
-color:'gray',
-fontSize:20,
-marginTop: 20,
+  itemCategoria: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 5,
+  },
 
+  colorBox: {
+    width: 20,
+    height: 20,
+    borderRadius: 4,
+    marginRight: 10,
+  },
 
-},
+  nombreCategoria: {
+    fontSize: 18,
+    color: '#d1d2daff',
+    fontWeight: '500',
+  },
 
+  row2: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
 
+  imagenBarras: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+      marginLeft: 40,
+  },
 
- row1:{
-   flexDirection:'row',
-  paddingHorizontal:15,
-  alignItems:'center',
-  width:'100%',
-  position:'relative',
-},
+  barras: {
+    width: 200,
+    height: 200,
+  },
 
-categorias: {
-  marginLeft: 50,
-  justifyContent: 'center',
-},
+  botonMes: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+    backgroundColor: '#a3abb3ff',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 6,
+  },
 
-itemCategoria: {
-  flexDirection: 'row',
-  alignItems: 'center',
-  marginVertical: 5,
-},
+  textoBoton: {
+    color: '#eef3f1ff',
+    fontSize: 14,
+    fontWeight: '600',
+  },
 
-colorBox: {
-  width: 20,
-  height: 20,
-  borderRadius: 4, 
-  marginRight: 10,
-},
+  listaInfo: {
+    marginTop: 25,
+    marginLeft: 25,
+    width: '85%',
+  },
 
-nombreCategoria: {
-  fontSize: 18,
-  color: '#d1d2daff',
-  fontWeight: '500',
-},
+  itemColor: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 6,
+    paddingLeft: 25,
+    paddingRight: 15,
+  },
 
-linea:{
+  nombreColor: {
+    flex: 1,
+    fontSize: 16,
+    color: '#171616ff',
+    fontWeight: 'bold',
+    marginLeft: 8,
+  },
 
-    height:2,
-    backgroundColor:'#393737ff',
-    marginVertical:20,
-    width:'100%',
-    alignSelf:'center',
-},
+  puntos: {
+    color: '#171616ff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    letterSpacing: 2,
+  },
 
-imagenBarras:{
-
-  justifyContent:'center',
-  alignItems: 'center',
-  
-},
-
-barras:{
-
-    width:250,
-    height:250,
-},
-
-
-row2:{
-
-  flexDirection:'row',
-  marginTop:10,
-  alignItems:'flex-start',
-
-},
-
-
-
-botonMes: {
-  backgroundColor: '#a3abb3ff',
-   paddingVertical: 5,
-  paddingHorizontal: 10,
-  borderRadius: 6,
-  marginLeft: 10,
-},
-
-textoBoton: {
-  color: '#eef3f1ff',
-  fontSize: 14,
-  fontWeight: '600',
-},
-
-itemColor: {
-  flexDirection: 'row',
-  alignItems: 'center',
-  marginRight: 25,  // espacio entre cada item
-},
-
-colorBox: {
-  width: 20,
-  height: 20,
-  borderRadius: 4,
-},
-
-nombreColor: {
-  fontSize: 16,
-  color: '#171616ff',
-  fontWeight:'bold',
-  marginLeft: 8, // espacio entre cuadro y texto
-},
-
-text1:{
-  fontWeight:'bold',
-  fontSize:20,
-
-
-
-}
-
-})
- 
+  textoFinal: {
+    color: 'gray',
+    fontSize: 18,
+    marginTop: 10,
+    marginBottom: 20,
+    marginLeft: 25,
+    width: '90%',
+  },
+});
--- AhorraAPP/Screens/MenuScreens.js
+++ AhorraAPP/Screens/MenuScreens.js
@@ -1,11 +1,9 @@
-import { Text, StyleSheet, View, Button, TouchableOpacity, SafeAreaView} from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import { Text, StyleSheet, View, Button } from 'react-native';
 import React, { useState } from 'react';
 import AutenticacionUsuario from './Autenticacion';
 import GestionDeTransacciones from './GestionTransacciones';
 import GraficasdeDatos from './Graficas';
 import Presupuestos from './PresupuestosMensuales';
-
 
 export default function Menu() {
     const[screen, SetScreen] = useState('menu');
@@ -22,107 +20,15 @@ export default function Menu() {
         case 'menu':
         default:
             return (
-              <SafeAreaView style={styles.container}> 
-                    
-                    <Text style={styles.title}>AhorraApp</Text>
-                     <Text style={styles.subtitle}> El control en la palma de tu mano</Text>
-                     <View style={styles.menuContainer}>
-                      <TouchableOpacity
-                       style={styles.boton}
-                       onPress={()=> SetScreen('gestion')}>
-                        <FontAwesome name="exchange" size={24} color="#fff" style={styles.icono}/>
-                        <Text style={styles.textoBoton}> Gestión de Transacciones  </Text>
-                       </TouchableOpacity>
-
-                      <TouchableOpacity
-                       style={styles.boton}
-                       onPress={()=> SetScreen('graficas')}>
-                        <FontAwesome name="pie-chart" size={24} color="#fff" style={styles.icono}/>
-                        <Text style={styles.textoBoton}> Gráficas </Text>
-                       </TouchableOpacity>
-
-                        <TouchableOpacity
-                       style={styles.boton}
-                       onPress={()=> SetScreen('presupuesto')}>
-                        <FontAwesome name="calendar" size={24} color="#fff" style={styles.icono}/>
-                        <Text style={styles.textoBoton}> Presupuestos mensuales </Text>
-                       </TouchableOpacity>
-                     
-                     </View>
-               
-               </SafeAreaView>  
-            );
+                <View style={styles.container}>
+                    <Text style={styles.title}>Menu Principal</Text>
+                    <Button onPress={() => SetScreen('Autenticacion')} title="Autenticacion de Usuario" />
+                    <Button onPress={() => SetScreen('gestion')} title="Gestion de Transacciones" />
+                    <Button onPress={() => SetScreen('graficas')} title="Graficas" />
+                    <Button onPress={() => SetScreen('presupuesto')} title="Presupuestos Mensuales" />
+                </View>
+            )
     }
 }
 
-const styles = StyleSheet.create({
-
-
-container:{
-flex:1,
-backgroundColor:'#f4f7fa',
-alignItems:'center',
-justifyContent:'flex-start',
-paddingTop:70,
-
-
-},
-
-title:{
-
-    fontSize:34,
-    fontWeight:'bold',
-    color:'#1a237e',
-    marginBottom:5,
-
-
-},
-
-subtitle:{
-    fontSize:16,
-    color:'#546e7a',
-    marginBottom:40,
-
-
-},
-
-menuContainer:{
-width:'85%',
-alignItems:'center',
-gap:20,
-
-},
-
-boton:{
- flexDirection:'row',
- alignItems:'center',
- backgroundColor:'#1e88e5',
- width:'100%',
- paddingVertical:14,
- borderRadius: 12,
- shadowColor:'#000',
- shadowOffset:{width:0,height:3},
- shadowOpacity:0.15,
- shadowRadius:5,
- elevation:5,
-
-},
-
-icono:{
-    marginLeft:20,
-    marginRight:15,
-},
-
-textoBoton:{
-
-color:'#fff',
-fontSize:18,
-fontWeight:'600',
-
-},
-
-
-
-
-
-})
+const styles = StyleSheet.create({})
